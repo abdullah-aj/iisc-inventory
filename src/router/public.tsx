c6401c6bf6b59b0fc97854dd976feaf7bbdf4c3a
Abdullah Mac HD <abdullah.exe@live.com>
2023-09-30 19:54:17 +0800

    link fix

--- src/router/public.tsx
+++ src/router/public.tsx
@@ -2,7 +2,7 @@ import React from 'react';
 import {createNativeStackNavigator} from '@react-navigation/native-stack';
 import {Login} from '../pages/Login';
 import {Splash} from '../pages/Splash';
-import { WelcomeScreen } from '../pages/WelcomeScreen';
+import {WelcomeScreen} from '../pages/WelcomeScreen';
 
 const PublicRoutes: React.FC = () => {
   const Stack = createNativeStackNavigator();
@@ -13,9 +13,9 @@ const PublicRoutes: React.FC = () => {
         screenOptions={{headerShown: false}}>
         <Stack.Screen name={'splash'} component={Splash} />
         <Stack.Screen name={'login'} component={Login} />
-        <Stack.Screen name='WelcomeScreen' component={WelcomeScreen} />
+        <Stack.Screen name="WelcomeScreen" component={WelcomeScreen} />
       </Stack.Navigator>
-    </> 
+    </>
   );
 };
 
